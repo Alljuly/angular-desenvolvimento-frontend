6f383450fc8ec6d58f4a20051c887076ad1289b6
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-22 20:04:30 -0300

    outros componentes

--- Fundamentos Angular/Trabalhando com Components/app/src/main.server.ts
+++ Fundamentos Angular/Trabalhando com Components/app/src/main.server.ts
@@ -1,7 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { MenuComponent } from './app/components/menu.component';
 import { config } from './app/app.config.server';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+//const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-export default bootstrap;
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, MenuComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
